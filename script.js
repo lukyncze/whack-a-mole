d9c31ed760296f53d013e2036c2aea4d89c508e5
Lukáš Sukeník <lukas.lukyn.sukenik@gmail.com>
2022-06-22 21:33:22 +0200

    fixed bug

--- script.js
+++ script.js
@@ -2,57 +2,52 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 const gameDuration = 10000;
+let lastHole,
+  gameInProgress,
+  score = 0;
 
-let lastHole;
-let score = 0;
-let timeUp = false;
-
-// Function that returns a random time
 const randomTime = (min, max) => Math.round(Math.random() * (max - min) + min);
 
-// Function that returns a random hole
 const randomHole = holes => {
-  const idx = Math.floor(Math.random() * holes.length);
-  const hole = holes[idx];
+  const number = Math.floor(Math.random() * holes.length);
+  const hole = holes[number];
+
   if (hole === lastHole) return randomHole(holes);
   lastHole = hole;
+
   return hole;
 };
 
-// Function for popping up moles
 const popUpMole = () => {
-  const time = randomTime(200, 1000);
+  const timeToPop = randomTime(200, 1000);
   const hole = randomHole(holes);
   hole.classList.add('up');
   setTimeout(() => {
     hole.classList.remove('up');
-    if (!timeUp) popUpMole();
-  }, time);
+    if (gameInProgress) popUpMole();
+  }, timeToPop);
 };
 
-// When user click the button -> start the game
-const startGame = () => {
-  // User can start only one game (Fixed bug)
-  let timestamp = +new Date();
-  if (this.timestamp && timestamp - this.timestamp < gameDuration) return;
-  this.timestamp = timestamp;
-
+const loadGame = () => {
   scoreBoard.textContent = 0;
-  timeUp = false;
   score = 0;
+  gameInProgress = true;
   popUpMole();
-  setTimeout(() => (timeUp = true), gameDuration);
+
+  setTimeout(() => (gameInProgress = false), gameDuration);
 };
 
-// Function that calls after user whacks a mole
-const moleHit = e => {
+const startGame = () => {
+  if (gameInProgress) return;
+  loadGame();
+};
+
+function moleHit(e) {
   // "Anti-cheat against players who likes to simulate their clicks"
   if (!e.isTrusted) return;
-
   this.parentNode.classList.remove('up');
   score++;
   scoreBoard.textContent = score;
-};
+}
 
-// Event listeners on whack a mole
 moles.forEach(mole => mole.addEventListener('click', moleHit));
